44ac3ae047449f42a0723c1a1aa211a82a69168a
mukund-gpt <abhishek.jupyter@gmail.com>
2025-02-10 18:58:35 +0530

    update socket connection URL and refactor base URL configuration

--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -1,2 +1,4 @@
 // export const baseUrl = "http://localhost:8000";
-export const baseUrl = "https://wavefront-backend.onrender.com";
+// export const socketUrl = "http://localhost:8000";
+export const baseUrl = "https://wavefront-backend.vercel.app";
+export const socketUrl = "https://wavefront-backend.onrender.com";
