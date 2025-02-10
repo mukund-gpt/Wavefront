37773e3ee385597452ed032a1acc44cef0001df7
mukund-gpt <abhishek.jupyter@gmail.com>
2025-02-10 19:16:24 +0530

    update CORS options to allow additional HTTP methods

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 const corsOptions = {
   origin: process.env.FRONTEND_URI,
+  methods: "GET,POST,PUT,DELETE,PATCH",
   credentials: true,
 };
 app.use(cors(corsOptions));
